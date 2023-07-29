43ecb0e08c910fa5a57148ff741396a3d49a02e0
MD Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2023-07-29 10:30:15 +0600

    add filter books in all books in fronted site

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,16 +1,66 @@
+import { useEffect, useState } from "react";
 import BooksCard from "../components/BooksCard/BooksCard";
 import Titles from "../components/Titles/Titles";
 import { useGetBooksQuery } from "../redux/api/apiSlice";
 
 const AllBooks = () => {
-    const { data } = useGetBooksQuery(undefined);
+    let { data } = useGetBooksQuery(undefined);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [books, setBooks] = useState([]);
+    useEffect(() => {
+        setBooks(data);
+        
+    },[data])
+    
+
+    const handleSearch = (e: any) => {
+        e.preventDefault();
+        setSearchTerm(e.target?.value);
+        
+            const filteredBooks = data.filter((book: any) => {
+                const lowerCaseTerm = searchTerm.toLowerCase();
+                return (
+                    book.book_name.toLowerCase().includes(lowerCaseTerm) ||
+                    book.author_name.toLowerCase().includes(lowerCaseTerm) ||
+                    book.genre.toLowerCase().includes(lowerCaseTerm)
+                );
+            });
+            console.log(filteredBooks);
+            if(filteredBooks.length >= 0){
+                setBooks(filteredBooks)
+            }
+    };
     return (
         <div className="py-10 dark:bg-slate-800">
             <Titles
                 title="All Books"
                 subTitle="Find all of your favorite books here"
             />
-            <BooksCard books={data} />
+            <div className="">
+                <div className="block md:flex justify-around w-full mb-6 px-10 space-y-4 md:space-y-0">
+                    <div>
+                        <label
+                            htmlFor="password"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                            Search Field
+                        </label>
+                        <input
+                            onChange={handleSearch}
+                            type="text"
+                            name="search"
+                            id="password"
+                            placeholder="Enter what you want to search"
+                            className=" lg:min-w-[400px] md:min-w-[300px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required
+                        />
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+            <BooksCard books={books} />
         </div>
     );
 };
